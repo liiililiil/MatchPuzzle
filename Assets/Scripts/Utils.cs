591c5a7966745038d3d847545034e1c99ae0ffbd
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-05-11 09:49:12 +0900

    상수값 변경

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 public static class Utils
 {
-    public const float TILE_SIZE = 1f;
-    public const float RAYCAST_LENGHT = 1f;
+    public const float TILE_SIZE = 0.25f;
+    public const float RAYCAST_LENGHT = 0.3f;
 
     //반복용
     public static readonly Vector2[] xDirections = { Vector2.left, Vector2.right };
