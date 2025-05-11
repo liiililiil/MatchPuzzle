e5df18ed216a7b400b32e90f0acb6cde592ded6c
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-05-11 09:50:50 +0900

    필드 접근 수정 및 Raycast 메서드 개선

--- Assets/Scripts/Playing/Tiles/Tile.cs
+++ Assets/Scripts/Playing/Tiles/Tile.cs
@@ -3,52 +3,22 @@ using UnityEngine;
 
 public abstract class Tile : MonoBehaviour, ITile
 {
-    private Chain _xChain;
-    private Chain _yChain;
-    private Chain _totalChain;
-    private bool _isCalculated;
-    public ushort xChainSelf
-    {
-        get => _xChain.self;
-        set => _xChain.self = value;
-    }
+    protected Chain _xChain;
+    protected Chain _yChain;
+    protected Chain _totalChain;
+    protected bool _isCalculated;
+    protected SpawnManager spawnManager;
 
-    public ushort yChainSelf
-    {
-        get => _yChain.self;
-        set => _yChain.self = value;
-    }
+    protected BoxCollider2D boxCollider2D;
 
-    public ushort totalChainSelf
-    {
-        get => _totalChain.self;
-        set => _totalChain.self = value;
-    }
-    
+    public ushort xChainSelf { get => _xChain.self; set => _xChain.self = value; } 
+    public ushort yChainSelf { get => _yChain.self; set => _yChain.self = value; } 
+    public ushort totalChainSelf { get => _totalChain.self; set => _totalChain.self = value; } 
+    public ushort xChainTotal { get => _xChain.total; set => _xChain.total = value; } 
+    public ushort yChainTotal { get => _yChain.total; set => _yChain.total = value; } 
+    public ushort totalChainTotal { get => _totalChain.total; set => _totalChain.total = value; } 
+    public bool isCalculated { get => _isCalculated; set => _isCalculated = value; }
 
-    public ushort xChainTotal
-    {
-        get => _xChain.total;
-        set => _xChain.total = value;
-    }
-
-    public ushort yChainTotal
-    {
-        get => _yChain.total;
-        set => _yChain.total = value;
-    }
-
-    public ushort totalChainTotal
-    {
-        get => _totalChain.total;
-        set => _totalChain.total = value;
-    }
-
-    public bool isCalculated
-    {
-        get => _isCalculated;
-        set => _isCalculated = value;
-    }
 
     public void ChainClear(){
         this.xChainSelf = 0;
@@ -59,76 +29,69 @@ public abstract class Tile : MonoBehaviour, ITile
         this.totalChainTotal = 0;
     }
 
-
-
+    public void Bind(SpawnManager spawnManager){
+        this.spawnManager = spawnManager;
+    }
+    public abstract void Calculate();
+    public abstract void NearbyCheck(ref Stack<ITile> totalStack, Vector2 exceptionDirection);
     
 
-    public void StartNearbyCheck(){
-        Stack<ITile> totalStack = new Stack<ITile>();
+    protected ITile Raycast(Vector2 direction, bool isGlobal)
+    {
+        // 박스 콜라이더 지정
+        if(boxCollider2D == null)
+            boxCollider2D = GetComponent<BoxCollider2D>();
 
-        NearbyCheck(ref totalStack, Vector2.zero);
+        // 자기 레이어만 감지하도록 설정
+        int layerMask;
 
-        ushort xMax = 0;
-        ushort yMax = 0;
-        ushort totalMax = 0;
+        // 레이어 마스크 설정
+        if(isGlobal) layerMask = -1;
+        else layerMask = 1 << gameObject.layer;
 
-        //최대값을 구합니다.
-        foreach (ITile tile in totalStack) {
-            xMax = (ushort)Mathf.Max(tile.xChainSelf, xMax);
-            yMax = (ushort)Mathf.Max(tile.yChainSelf, yMax);
-            totalMax = (ushort)Mathf.Max(tile.totalChainSelf, totalMax);
-        }
+        // 로컬 기준 direction을 월드 방향으로 변환
+        Vector2 worldDirection = transform.TransformDirection(direction);
 
-        foreach (ITile tile in totalStack) {
-            tile.xChainTotal = xMax;
-            tile.yChainTotal = yMax;
-            tile.totalChainTotal = totalMax;
-        }
+        //사격전 자신은 제외시키기
+        
+
+        // Raycast 수행
+        // Debug.Log("Raycast from: " + transform.position + " to: " +  worldDirection + " with layerMask: " + layerMask);
+        boxCollider2D.enabled = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, worldDirection, Utils.RAYCAST_LENGHT, layerMask);
+        boxCollider2D.enabled = true;
+
+        // Debug.Log("Raycast hit: " + hit.collider);
+
+
+        // 결과 반환
+        return hit ? hit.collider.GetComponent<ITile>() : null;
+    }
+
+    public  void ForceBlasted(){
+        Destroy(this);
 
     }
 
-    public void NearbyCheck(ref Stack<ITile> totalStack, Vector2 exceptionDirection){
-        totalStack.Push(this);
-        isCalculated = true;
+    public abstract void Blasted();
+    public abstract void Drop();
 
-        //초기화
-        ChainClear();
+    private void OnCollisionEnter(Collision collision){
+        //자신은 강제 삭제
+        ForceBlasted();
 
-        //X축 방향으로 검사
-        foreach (var direction in Utils.xDirections){
-
-            ITile tile = Raycast(direction);
-            if (tile != null && !tile.isCalculated){
-                this.xChainSelf = (ushort)Mathf.Max(tile.xChainSelf + 1, this.xChainSelf);
-                this.totalChainSelf = (ushort)Mathf.Max(tile.totalChainSelf + 1, this.totalChainSelf);
-
-                //재귀적으로
-                if (direction != exceptionDirection) tile.NearbyCheck(ref totalStack, -direction);
-            }
-        }
-
-        //Y축 방향으로 검사
-        foreach (var direction in Utils.yDirections){
-            if (direction == exceptionDirection) continue;
-
-            ITile tile = Raycast(direction);
-            if (tile != null && !totalStack.Contains(tile)){
-                this.yChainSelf = (ushort)Mathf.Max(tile.yChainSelf + 1, this.yChainSelf);
-                this.totalChainSelf = (ushort)Mathf.Max(tile.totalChainSelf + 1, this.totalChainSelf);
-
-                //재귀적으로
-                if (direction != exceptionDirection) tile.NearbyCheck(ref totalStack, -direction);
-            }
-        }
+        //자신의 위 타일에게 낙하을 요청
+        ITile tile = Raycast(Vector2.up, true);
+        tile?.Drop();
     }
 
-    
-
-    private ITile Raycast(Vector2 direction){
-        int myLayerMask = 1 << gameObject.layer;
-
-        RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, direction, Utils.RAYCAST_LENGHT, myLayerMask);
-
-        return hit? hit.collider.gameObject.GetComponent<ITile>() : null;
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + (new Vector3(1, 0) * Utils.RAYCAST_LENGHT));
+        Gizmos.DrawLine(transform.position, transform.position + (new Vector3(-1, 0) * Utils.RAYCAST_LENGHT));
+        Gizmos.DrawLine(transform.position, transform.position + (new Vector3(0, 1) * Utils.RAYCAST_LENGHT));
+        Gizmos.DrawLine(transform.position, transform.position + (new Vector3(0, -1) * Utils.RAYCAST_LENGHT));
+        Gizmos.DrawCube(transform.position, new Vector3(Utils.TILE_SIZE, Utils.TILE_SIZE, 1));
     }
 }
