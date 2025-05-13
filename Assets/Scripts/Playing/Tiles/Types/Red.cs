e6561538a5667afaefa00837c1257ff035e4d755
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-05-14 07:00:17 +0900

    타일 종류 정의

--- Assets/Scripts/Playing/Tiles/Types/Red.cs
+++ Assets/Scripts/Playing/Tiles/Types/Red.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Red : ColorTile, ITile
 {
     public bool cal = false;
+    public override sealed TileType tileType {get => TileType.Red;} 
 
     void Update()
     {
