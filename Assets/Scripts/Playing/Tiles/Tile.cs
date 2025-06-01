b94162327d3f59ad66d413f002c55dd47023d517
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-02 07:56:40 +0900

    Tile 클래스에서 초기화 메서드 및 불필요한 필드 제거, Raycast 메서드 수정

--- Assets/Scripts/Playing/Tiles/Tile.cs
+++ Assets/Scripts/Playing/Tiles/Tile.cs
@@ -9,17 +9,18 @@ public abstract class Tile : MonoBehaviour, ITile
     protected Chain _xChain = new Chain();
     protected Chain _yChain = new Chain();
     protected Chain _totalChain = new Chain();
-    protected bool _isCalculated;
-    protected SpawnManager spawnManager;
-    protected EventManager eventManager;
-    protected BoxCollider2D boxCollider2D;
+    protected byte bitFlag;
+    protected CircleCollider2D circleCollider2D;
+    protected new Rigidbody2D rigidbody2D;
+    
 
 
     private Vector2 direction;
     public Chain xChain { get => _xChain; set => _xChain = value; }
     public Chain yChain { get => _yChain; set => _yChain = value; }
     public Chain totalChain { get => _totalChain; set => _totalChain = value; }
-    public bool isCalculated { get => _isCalculated; set => _isCalculated = value; }
+    public bool isCalculated { get => (bitFlag & 1 << 0) != 0; set => bitFlag = (byte)(value ? bitFlag | 1<<0 : bitFlag & ~(1<<0)); }
+    // public bool isDrop { get => (bitFlag & 1 << 1) != 0; set => bitFlag = (byte)(value ? bitFlag | 1<<1 : bitFlag & ~(1<<1)); }
 
     public abstract TileType tileType { get; }
 
@@ -31,17 +32,8 @@ public abstract class Tile : MonoBehaviour, ITile
 
     }
 
-    public void Initialize(SpawnManager spawnManager, EventManager eventManager)
+    protected ITile Raycast(Vector2 direction, float lenghtMultiple, bool isGlobal)
     {
-        this.spawnManager = spawnManager;
-        this.eventManager = eventManager;
-        boxCollider2D = GetComponent<BoxCollider2D>();
-    }
-    protected ITile Raycast(Vector2 direction, int lenghtMultiple, bool isGlobal)
-    {
-        if (boxCollider2D == null)
-            boxCollider2D = GetComponent<BoxCollider2D>();
-
         int layerMask;
         if (isGlobal) layerMask = -1;
         else layerMask = 1 << gameObject.layer;
@@ -55,7 +47,7 @@ public abstract class Tile : MonoBehaviour, ITile
         Debug.DrawRay(start, worldDirection * (Utils.RAYCASY_LENGHT * lenghtMultiple), Color.red, 0.1f);
         RaycastHit2D hit = Physics2D.Raycast(start, worldDirection, Utils.RAYCASY_LENGHT * lenghtMultiple, layerMask);
 
-        return hit ? hit.collider.GetComponent<ITile>() : null;
+        return hit.collider ? hit.collider.GetComponent<ITile>() : null;
     }
 
 
@@ -67,14 +59,13 @@ public abstract class Tile : MonoBehaviour, ITile
     public void pooling()
     {
         gameObject.transform.position = new Vector2(Utils.WAIT_POS_X, Utils.WAIT_Pos_Y);
-        spawnManager.Pooling(gameObject, tileType);
+        SpawnManager.Instance.Pooling(gameObject, tileType);
 
     }
 
     // public abstract void MoveMent(Vector2 target);
 
     public abstract void Blasted();
-    public abstract void Drop();
     public abstract void Calculate();
     public abstract void NearbyCheck(ref Stack<ITile> totalStack, Vector2 exceptionDirection);
 
@@ -85,10 +76,6 @@ public abstract class Tile : MonoBehaviour, ITile
     {
         //자신은 강제 삭제
         ForceBlasted();
-
-        //자신의 위 타일에게 낙하을 요청
-        ITile tile = Raycast(Vector2.up, 1, true);
-        tile?.Drop();
     }
 
     void OnDrawGizmos()
