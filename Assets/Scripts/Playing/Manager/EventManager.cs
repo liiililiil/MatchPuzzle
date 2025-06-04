48af4da9c47639f100dea48e4bffdb66760a35ba
Todcm <109393040+liiililiil@users.noreply.github.com>
2025-06-04 13:00:51 +0900

    이벤트를 액션으로 변경 및 이벤트 추가

--- Assets/Scripts/Playing/Manager/EventManager.cs
+++ Assets/Scripts/Playing/Manager/EventManager.cs
@@ -6,12 +6,19 @@ using UnityEngine.Events;
 public class EventManager : MonoBehaviour
 {
     public static EventManager Instance { get; private set; }
+    public Action OnCalReset;
+    public Action OnCalculate;
 
-    [HideInInspector]
-    public UnityEvent OnCalReset = new UnityEvent();
+    //타일 전체에 대한 액션
+    public Action<ITile, Vector2> OnDisableTile;
+    public Action<ITile, Vector2> OnBlastTile;
+    public Action<ITile, ITileDestroyer, Vector2> OnBlastTileByBomb;
+    public Action<ITile, Vector2> OnBombActive;
+    public Action<ITile, Vector2> OnOrganized;
+    public Action<ITile, Vector2> OnSpawnTile;
 
-    [HideInInspector]
-    public UnityEvent OnCalculate = new UnityEvent();
+
+
     void Awake(){   
         //싱글톤
         if (Instance == null){
