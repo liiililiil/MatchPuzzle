b17541d3cd08f491e5a1fccf2e79390bbf22b8ca
Todcm <109393040+liiililiil@users.noreply.github.com>
2025-06-05 16:20:10 +0900

    상수 조정

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -4,12 +4,13 @@ public static class Utils
 {
     public const float TILE_SIZE = 0.6f;
     public const float TILE_GAP = 0.6f;
-    public const float RAYCASY_LENGHT = 0.5f;
-    public const float RAYCASY_REVISION = 0.15f; 
+    public const float RAYCASY_LENGHT = 0.4f;
+    public const float RAYCASY_REVISION = 0.4f; 
     public const float WAIT_POS_X = 100f;
     public const float WAIT_Pos_Y = 100f;
     public const float ACCELERATION_SPEED = 50f;
     public const float MOVEMENT_SPEED = 20f;
+    public const int TILETYPE_LENGHT = 9;
 
     // public const string StagePath = @"\Stage\"
 
