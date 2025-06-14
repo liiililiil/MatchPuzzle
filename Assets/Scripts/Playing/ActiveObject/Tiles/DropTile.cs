fd1dd56c8c79c816637a475cc4031448a89d4af1
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-14 20:33:06 +0900

    정리, RayCast 로직 변경

--- Assets/Scripts/Playing/ActiveObject/Tiles/DropTile.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/DropTile.cs
@@ -22,55 +22,34 @@ public abstract class DropTile : Tile, ITile
 
     protected void DropCalculation()
     {
-
-
         //밑으로 하강 연산
-        ITile belowTile = Raycast(Vector2.down, 1, true);
+        ITile belowTile = GetTileFromWorld(Vector2.down, true);
         if (DropTest(belowTile, -transform.up)) return;
 
         //옆으로 하강 연산
         foreach (Vector2 dir in Utils.xDirections)
         {
-            belowTile = Raycast(Vector2.down + dir, 1.7f, true);
+            belowTile = GetTileFromWorld(Vector2.down + dir, true);
 
-            //옆으로 하강하기전에 떨어질 타일이 있으면 떨어지지않음
-            ITile sideTile = Raycast(dir, 1, true);
+            //옆으로 하강하기전에 떨어질 타일이 있으면 떨어지지 않음
+            ITile sideTile = GetTileFromWorld(dir, true);
             if (sideTile != null && sideTile.isCanDrop)
             {
-                // sideTile.Drop();
                 continue;
             }
 
-            Vector2 worldDirection = transform.TransformDirection(dir);
-
-            // 콜라이더 바깥으로 Raycast 시작 지점 계산
-            Vector2 start = (Vector2)transform.position + worldDirection * (Utils.RAYCASY_REVISION * 3);
-
-            // Raycast 수행
-            Debug.DrawRay(start, worldDirection * Utils.RAYCASY_LENGHT, Color.blue, 0.01f);
-            RaycastHit2D hit = Physics2D.Raycast(start, worldDirection, Utils.RAYCASY_LENGHT, -1);
-            
-            ITile tile = hit.collider ? hit.collider.GetComponent<ITile>() : null;
-
-
-
+            //대각선으로 떨어질 타일이 있으면 떨어지지 않음
+            sideTile = GetTileFromWorld(dir, 2, true);
             if (sideTile != null && sideTile.isDrop)
             {
                 continue;
             }
 
+            //떨어트리기
             if (DropTest(belowTile, -transform.up + (dir == Vector2.right ? transform.right : -transform.right))) return;
         }
+        
 
-        // // 이동했었고 이동이 끝났으면 위 타일에게 하강 호출
-        // if (needCallDrop)
-        // {
-        //     ITile[] aboveTile = { Raycast(Vector2.up, 3, true), Raycast(Vector2.up + Vector2.right, 5f, true), Raycast(Vector2.up + Vector2.left, 5f, true) };
-        //     foreach (ITile tile in aboveTile) if (tile != null && !tile.isDrop) tile.Drop();
-
-        //     //본인은 계산대기
-        //     EventManager.Instance.OnCalculate += Calculate;
-        // }
     }
 
     protected bool DropTest(ITile belowTile, Vector3 dir)
@@ -78,7 +57,7 @@ public abstract class DropTile : Tile, ITile
         if (belowTile == null || belowTile.isDrop)
         {
             //일단 위에 타일 구하기
-            ITile[] aboveTile = { Raycast(Vector2.up, 3f, true), Raycast(Vector2.up + Vector2.right, 3f, true), Raycast(Vector2.up + Vector2.left, 3f, true) };
+            ITile[] aboveTile = { GetTileFromWorld(Vector2.up, 2, true), GetTileFromWorld(Vector2.up + Vector2.right, 2, true), GetTileFromWorld(Vector2.up + Vector2.left, 2, true) };
 
             //이동
             isDrop = true;
@@ -121,12 +100,14 @@ public abstract class DropTile : Tile, ITile
 
         // 위치 정밀 보정
         transform.position = targetPos;
+
+        EventManager.Instance.movingTiles--;
         coroutineEnd();
+
     }
 
     protected void coroutineEnd()
     {
-        EventManager.Instance.movingTiles--;
 
         StopCoroutine(coroutine);
         coroutine = null;
