efbb4c897f12eee7ce20f7d626983fcc832c86b7
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-16 00:36:43 +0900

    계산 로직 교체

--- Assets/Scripts/Playing/ActiveObject/Tiles/ColorTile.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/ColorTile.cs
@@ -10,46 +10,51 @@ public abstract class ColorTile : DropTile, ITile
         if (isCalculated) return;
 
         Stack<ITile> totalStack = new Stack<ITile>();
-
-        NearbyCheck(ref totalStack, Vector2.zero);
+        NearbyCheck(ref totalStack, Vector2.zero, 0);
 
         ushort xMax = 0;
         ushort yMax = 0;
-        ushort totalMax = 0;
+        // ushort totalMax = 0;
 
         Debug.Log("totalStack Count: " + totalStack.Count);
+        int count = totalStack.Count;
 
         //최대값을 구합니다.
         foreach (ITile tile in totalStack)
         {
             xMax = (ushort)Mathf.Max(tile.xChain.self, xMax);
             yMax = (ushort)Mathf.Max(tile.yChain.self, yMax);
-            totalMax = (ushort)Mathf.Max(tile.totalChain.self, totalMax);
+            // totalMax = (ushort)Mathf.Max(tile.totalChain.self, totalMax);
         }
 
-        foreach (ITile tile in totalStack)
+        while (totalStack.Count > 0)
         {
+            ITile tile = totalStack.Pop();
             tile.xChain.total = xMax;
             tile.yChain.total = yMax;
-            tile.totalChain.total = totalMax;
+            // tile.totalChain.total = totalMax;
 
             EventManager.Instance.OnCalReset += tile.CalReset;
 
             //터지는 조건이 되면 폭발
-
-            Debug.Log(tile.totalChain.total);
-            if (tile.totalChain.total >= 3)
+            if (count >= 3)
             {
-                tile.Blasted();
+                if (totalStack.Count <= 1) tile.Blasted(true);
+                else tile.Blasted();
             }
         }
 
     }
 
-    public override sealed void NearbyCheck(ref Stack<ITile> totalStack, Vector2 exceptionDirection)
+    public override sealed void NearbyCheck(ref Stack<ITile> totalStack, Vector2 exceptionDirection, ushort chain)
     {
         totalStack.Push(this);
         isCalculated = true;
+
+        //제외된 방향을 기반으로 체인을 증가
+        if (exceptionDirection == Vector2.zero);
+        else if (exceptionDirection == new Vector2(0, exceptionDirection.y)) yChain.self = chain;
+        else if (exceptionDirection == new Vector2(exceptionDirection.x, 0)) xChain.self = chain;
 
 
         //X축 방향으로 검사
@@ -59,11 +64,15 @@ public abstract class ColorTile : DropTile, ITile
             ITile tile = GetTileFromWorld(direction);
             if (tile != null)
             {
-                this.xChain.self = (ushort)Mathf.Max(tile.xChain.self + 1, this.xChain.self);
-                this.totalChain.self = (ushort)Mathf.Max(tile.totalChain.self + 1, this.totalChain.self);
+                // this.xChain.self = (ushort)Mathf.Max(tile.xChain.self + 1, xChain.self);
+                // this.totalChain.self = (ushort)Mathf.Max(tile.totalChain.self + 1, totalChain.self);
 
                 //재귀적으로
-                if (direction != exceptionDirection && !tile.isCalculated) tile.NearbyCheck(ref totalStack, -direction);
+                if (direction != exceptionDirection && !tile.isCalculated)
+                {
+                    tile.NearbyCheck(ref totalStack, -direction, (ushort)(xChain.self + 1));
+                    xChain += tile.xChain;
+                }
             }
         }
 
@@ -75,17 +84,21 @@ public abstract class ColorTile : DropTile, ITile
             ITile tile = GetTileFromWorld(direction);
             if (tile != null)
             {
-                this.yChain.self = (ushort)Mathf.Max(tile.yChain.self + 1, this.yChain.self);
-                this.totalChain.self = (ushort)Mathf.Max(tile.totalChain.self + 1, this.totalChain.self);
+                // this.yChain.self = (ushort)Mathf.Max(tile.yChain.self + 1, yChain.self);
+                // this.totalChain.self = (ushort)Mathf.Max(tile.totalChain.self + 1, totalChain.self);
 
                 //재귀적으로
-                if (direction != exceptionDirection && !tile.isCalculated) tile.NearbyCheck(ref totalStack, -direction);
+                if (direction != exceptionDirection && !tile.isCalculated)
+                {
+                    tile.NearbyCheck(ref totalStack, -direction, (ushort)(yChain.self + 1));
+                    yChain += tile.yChain;
+                }
             }
         }
     }
 
     public sealed override void Organize() { } //색 타일은 정리당하지 않음
-    public sealed override void Blasted()
+    public sealed override void Blasted(bool isCenter = false)
     {
         EventManager.Instance.OnBlastedTile.Invoke(this, transform.position);
 
@@ -99,9 +112,9 @@ public abstract class ColorTile : DropTile, ITile
             tile?.Organize();
         }
 
-        CalculateSpawnBomb();
+        if(isCenter) CalculateSpawnBomb();
 
-        Debug.Log("터지는 조건 충족");
+        // Debug.Log("터지는 조건 충족");
         Disable();
     }
 
