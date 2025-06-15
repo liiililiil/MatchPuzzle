e979225ccc17dbc6fb683918a2d3d3063106075b
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-16 00:36:58 +0900

    isDrop 사용

--- Assets/Scripts/Playing/ActiveObject/Tiles/ITile.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/ITile.cs
@@ -6,18 +6,18 @@ public interface ITile
 {
     public Chain xChain { get; set; }
     public Chain yChain { get; set; }
-    public Chain totalChain { get; set; }
+    // public Chain totalChain { get; set; }
     public TileType tileType { get; }
     public bool isCanDrop { get; }
 
     public bool isCalculated { get; set; }
-    // public bool isDrop { get; set; }
+    public bool isDrop { get; set; }
     
 
 
     public void Calculate();
-    public void NearbyCheck(ref Stack<ITile> totalStack, Vector2 exceptionDirection);
-    public void Blasted();
+    public void NearbyCheck(ref Stack<ITile> totalStack, Vector2 exceptionDirection, ushort chain);
+    public void Blasted(bool isCenter = false);
     public void ForceBlasted();
     public void Organize();
     public void CalReset();
