f1e787e327c9ab7d6da80ba66ce03587fdbe55f2
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-15 22:09:42 +0900

    버그 수정 및 로직 수정

--- Assets/Scripts/Playing/ActiveObject/Tiles/DropTile.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/DropTile.cs
@@ -9,14 +9,14 @@ public abstract class DropTile : Tile, ITile
     public override sealed void Drop()
     {
         //이미 이동중인데 함수 호출되면 반환
-        if (isDrop) return;
+        // if (isDrop) return;
 
         //코루틴 실행중이라면 반환 
         if (isCoroutineRunning()) return;
 
         if (boxCollider2D == null) boxCollider2D = this.GetComponent<BoxCollider2D>();
 
-        isDrop = false;
+        // isDrop = false;
         EventManager.Instance.OnDrop += DropCalculation;
     }
 
@@ -30,23 +30,30 @@ public abstract class DropTile : Tile, ITile
         foreach (Vector2 dir in Utils.xDirections)
         {
             belowTile = GetTileFromWorld(Vector2.down + dir, true);
-
-            //옆으로 하강하기전에 떨어질 타일이 있으면 떨어지지 않음
-            ITile sideTile = GetTileFromWorld(dir, true);
-            if (sideTile != null && sideTile.isCanDrop)
+            if (belowTile == null)
             {
-                continue;
+                //옆으로 하강하기전에 떨어질 타일이 있으면 떨어지지 않음
+                ITile sideTile = GetTileFromWorld(dir, true);
+                if (sideTile != null && sideTile.isCanDrop)
+                {
+                    continue;
+                }
+
+                //오른쪽으로 떨어질 타일이 있으면 떨어지지 않음
+                //
+                if (dir == Vector2.right)
+                {
+                    sideTile = GetTileFromWorld(dir, 2, true);
+                    if (sideTile != null /*&& sideTile.isDrop*/)
+                        continue;
+                
+                }
+
+                //떨어트리기
+                if (DropTest(belowTile, -transform.up + (dir == Vector2.right ? transform.right : -transform.right))) return;
             }
 
-            //대각선으로 떨어질 타일이 있으면 떨어지지 않음
-            sideTile = GetTileFromWorld(dir, 2, true);
-            if (sideTile != null && sideTile.isDrop)
-            {
-                continue;
-            }
 
-            //떨어트리기
-            if (DropTest(belowTile, -transform.up + (dir == Vector2.right ? transform.right : -transform.right))) return;
         }
         
 
@@ -54,20 +61,11 @@ public abstract class DropTile : Tile, ITile
 
     protected bool DropTest(ITile belowTile, Vector3 dir)
     {
-        if (belowTile == null || belowTile.isDrop)
+        if (belowTile == null /*|| belowTile.isDrop*/)
         {
-            //일단 위에 타일 구하기
-            ITile[] aboveTile = { GetTileFromWorld(Vector2.up, 2, true), GetTileFromWorld(Vector2.up + Vector2.right, 2, true), GetTileFromWorld(Vector2.up + Vector2.left, 2, true) };
-
             //이동
-            isDrop = true;
-            // needCallDrop = true;
-
             EventManager.Instance.movingTiles++;
             coroutine = StartCoroutine(moveMent2D(dir));
-
-            //위 하강 등록
-            foreach (ITile tile in aboveTile) if (tile != null && !tile.isDrop) tile.Drop();
 
             return true;
 
@@ -78,9 +76,12 @@ public abstract class DropTile : Tile, ITile
 
     IEnumerator moveMent2D(Vector2 direction)
     {
+        //일단 위에 타일 구하기
+        ITile[] aboveTile = { GetTileFromWorld(Vector2.up, true), GetTileFromWorld(Vector2.up + Vector2.right, true), GetTileFromWorld(Vector2.up + Vector2.left, true) };
+
 
         yield return null;
-        isDrop = false;
+        // isDrop = false;
 
         // 현재 위치를 그리드에 정렬
         Vector2 startPos = new Vector2(
@@ -105,6 +106,9 @@ public abstract class DropTile : Tile, ITile
         sprite.transform.position = targetPos;
 
         EventManager.Instance.movingTiles--;
+
+        //위 하강 등록
+        foreach (ITile tile in aboveTile) if (tile != null /*&& !tile.isDrop*/) tile.Drop();
         coroutineEnd();
 
     }
