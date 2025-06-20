8a2c0586625e5284d3b6720f62b8cfbb04794fda
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-21 07:46:30 +0900

    코드 정리에 의한 변경

--- Assets/Scripts/Playing/ActiveObject/Tiles/NonDropTile.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class NonDropTile : Tile, ITile
-{
-    public override bool isCanDrop { get => false; }
-    public sealed override void Drop()
-    {
-    }
-}
