9790b65f9dcba21c1ca54eb8d73e4315c5ad89e4
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-21 07:47:59 +0900

    타일 사이즈 조정 및 테스트용 상수 정의

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Runtime.CompilerServices;
 public static class Utils
 {
-    public const float TILE_SIZE = 1f;
+    public const float TILE_SIZE = 0.9f;
     public const float TILE_GAP = 1f;
     public const float RAYCASY_LENGHT = 0.2f;
     public const float RAYCASY_REVISION = 1f;
@@ -14,6 +14,8 @@ public static class Utils
     public const int EFFECTTYPE_LENGHT = 1;
     public const float EFFECT_SPEED = 20f;
 
+    public const int MOVEMENT_DURATION = 100; // 이동 지속 시간 (ms)
+
     // public const string StagePath = @"\Stage\"
 
 
@@ -21,12 +23,14 @@ public static class Utils
     //반복용
     public static readonly Vector2[] xDirections = { Vector2.left, Vector2.right };
     public static readonly Vector2[] yDirections = { Vector2.up, Vector2.down };
+    public static readonly Vector2[] directions = { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2 FloatToVector2(float value)
     {
         return new Vector2(value, value);
     }
+    
 
     
 
