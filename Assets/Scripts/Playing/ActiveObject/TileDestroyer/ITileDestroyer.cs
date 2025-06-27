e08cc8f37bf0b2f84975cfa410b58210f1fd8990
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-28 06:19:18 +0900

    정리

--- Assets/Scripts/Playing/ActiveObject/TileDestroyer/ITileDestroyer.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Mathematics;
-using UnityEngine;
-
-public interface ITileDestroyer
-{
-    public Tile startBy { get; set; }
-    public void Fire(Tile startBy, Vector2 pos, quaternion rotate);
-}
