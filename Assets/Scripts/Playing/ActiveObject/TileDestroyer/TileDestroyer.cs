177cad7c5caf2a80df3463aeca9de989134795c6
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-06-28 06:19:40 +0900

    TileDestroyer 클래스의 구조 개선 및 Enable/Disable 메서드 수정

--- Assets/Scripts/Playing/ActiveObject/TileDestroyer/TileDestroyer.cs
+++ Assets/Scripts/Playing/ActiveObject/TileDestroyer/TileDestroyer.cs
@@ -1,13 +1,69 @@
-using Unity.Mathematics;
 using UnityEngine;
 
-public abstract class TileDestroyer : MonoBehaviour, ITileDestroyer
+public class TileDestroyer : MonoBehaviour, IActiveObject
 {
-    public Tile startBy { get; set; }
-    public void Fire(Tile startBy, Vector2 pos, quaternion rotate)
+    public TileType startBy { get; private set; }
+    public DestroyerType type;
+    public bool isActive { get; set; }
+
+    private IMovementAction movementAction;
+    private IDestroyAction destroyAction;
+    private IExtinctionAction extinctionAction;
+
+    private void Awake()
     {
-        this.startBy = startBy;
+        InitAction(out movementAction, "IMovementAction");
+        InitAction(out destroyAction, "IDestroyAction");
+        InitAction(out extinctionAction, "IExtinctionAction");
+    }
+    public void Fire(Tile startBy, Vector2 pos, Quaternion rotate)
+    {
+        // this.startBy = startBy;
         transform.position = pos;
         transform.rotation = rotate;
     }
+
+    private void InitAction<T>(out T action, string actionName) where T : IDestroyerAction
+    {
+        action = GetComponent<T>();
+        if (action == null)
+        {
+            Debug.LogError($"{gameObject.name} 에게 {actionName} 컴포넌트가 없습니다!");
+        }
+        else if (action is IDestroyerAction)
+        {
+            action.Init(this);
+        }
+    }
+
+    public void Enable(Vector2 position, Quaternion rotate, IActiveObject startBy = null)
+    {
+        if(startBy == null) Debug.LogWarning("이 타일 소멸자의 시작지점이 설정되지 못하였습니다!");
+        transform.position = position;
+        transform.rotation = rotate;
+        isActive = true;
+
+        movementAction?.Invoke();
+        destroyAction?.Invoke();
+        extinctionAction?.Invoke();
+
+    }
+    
+    public void Disable()
+    {
+        isActive = false;
+
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+        if (boxCollider2D != null)
+            boxCollider2D.enabled = false;
+        else
+            Debug.LogError($"{gameObject.name} 에게 BoxCollider2D가 없습니다!");
+
+        EventManager.Instance.OnSpawnedActiveOjbect.Invoke(this, transform.position);
+
+        transform.position = new Vector2(Utils.WAIT_POS_X, Utils.WAIT_Pos_Y);
+        transform.rotation = Quaternion.identity;
+        // SpawnManager.Instance.pooling(gameObject, this);
+    }
+
 }
