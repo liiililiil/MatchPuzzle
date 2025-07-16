2f732b9b6953d28d36462ab69524df08a0be41ac
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-07-17 08:29:06 +0900

    개선

--- Assets/Scripts/Playing/ActiveObject/TileDestroyer/TileDestroyer.cs
+++ Assets/Scripts/Playing/ActiveObject/TileDestroyer/TileDestroyer.cs
@@ -4,18 +4,14 @@ public class TileDestroyer : MonoBehaviour, IActiveObject
 {
     public TileType startBy { get; private set; }
     public DestroyerType type;
-    public bool isActive { get; set; }
+    public bool isActive { get; private set; }
+    private bool isInit;
 
     private IMovementAction movementAction;
     private IDestroyAction destroyAction;
     private IExtinctionAction extinctionAction;
-
-    private void Awake()
-    {
-        InitAction(out movementAction, "IMovementAction");
-        InitAction(out destroyAction, "IDestroyAction");
-        InitAction(out extinctionAction, "IExtinctionAction");
-    }
+    private ISpriteAction spriteAction;
+    private IEffectSpawnAction effectAction;
     public void Fire(Tile startBy, Vector2 pos, Quaternion rotate)
     {
         // this.startBy = startBy;
@@ -23,12 +19,12 @@ public class TileDestroyer : MonoBehaviour, IActiveObject
         transform.rotation = rotate;
     }
 
-    private void InitAction<T>(out T action, string actionName) where T : IDestroyerAction
+    private void InitAction<T>(out T action) where T : IDestroyerAction
     {
         action = GetComponent<T>();
         if (action == null)
         {
-            Debug.LogError($"{gameObject.name} 에게 {actionName} 컴포넌트가 없습니다!");
+            Debug.LogError($"{gameObject.name} 에게 {typeof(T)} 컴포넌트가 없습니다!");
         }
         else if (action is IDestroyerAction)
         {
@@ -38,19 +34,43 @@ public class TileDestroyer : MonoBehaviour, IActiveObject
 
     public void Enable(Vector2 position, Quaternion rotate, IActiveObject startBy = null)
     {
-        if(startBy == null) Debug.LogWarning("이 타일 소멸자의 시작지점이 설정되지 못하였습니다!");
+        EventManager.Instance.activeDestroyer++;
+        if (!isInit)
+        {
+            isInit = true;
+            InitAction(out movementAction);
+            InitAction(out destroyAction);
+            InitAction(out extinctionAction);
+            InitAction(out spriteAction);
+            InitAction(out effectAction);
+        }
+        
+        if (startBy == null) Debug.LogWarning("이 타일 소멸자의 시작지점이 설정되지 못하였습니다!");
         transform.position = position;
         transform.rotation = rotate;
         isActive = true;
 
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+        if (boxCollider2D != null)
+            boxCollider2D.enabled = true;
+        else
+            Debug.LogError($"{gameObject.name} 에게 BoxCollider2D가 없습니다!");
+
+            
+        EventManager.Instance.OnSpawnedActiveOjbect.Invoke(this, transform.position);
+
         movementAction?.Invoke();
         destroyAction?.Invoke();
         extinctionAction?.Invoke();
+        spriteAction?.Invoke();
+        effectAction?.Invoke();
 
     }
     
     public void Disable()
     {
+        EventManager.Instance.activeDestroyer--;
+
         isActive = false;
 
         BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
@@ -59,11 +79,11 @@ public class TileDestroyer : MonoBehaviour, IActiveObject
         else
             Debug.LogError($"{gameObject.name} 에게 BoxCollider2D가 없습니다!");
 
-        EventManager.Instance.OnSpawnedActiveOjbect.Invoke(this, transform.position);
 
         transform.position = new Vector2(Utils.WAIT_POS_X, Utils.WAIT_Pos_Y);
         transform.rotation = Quaternion.identity;
-        // SpawnManager.Instance.pooling(gameObject, this);
+
+        SpawnManager.Instance.Pooling(type, gameObject);
     }
 
 }
