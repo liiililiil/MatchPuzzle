bc75ede6a2fce50a6266f1f5e9e1e9908a1a90a7
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-07-17 06:51:31 +0900

    삭제됨

--- Assets/Scripts/Playing/ActiveObject/Effect/DisabledEffect.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using Unity.Mathematics;
-using UnityEngine;
-
-public class DisabledEffect : Effect, IEffect
-{
-    public override EffectType effectType { get => EffectType.Disabled; }
-}
--- Assets/Scripts/Playing/ActiveObject/Effect/IEffect.cs
+++
@@ -1,4 +0,0 @@
-public interface IEffect : IActiveObject
-{
-    public EffectType effectType { get; }
-}
