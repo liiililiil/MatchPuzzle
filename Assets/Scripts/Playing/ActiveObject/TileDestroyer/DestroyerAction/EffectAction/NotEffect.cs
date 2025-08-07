3d256646031bb926e215e30e79c3a16211b6dd94
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-08-08 01:01:04 +0900

    효과없는 이펙트 생성

---
+++ Assets/Scripts/Playing/ActiveObject/TileDestroyer/DestroyerAction/EffectAction/NotEffect.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class NotEffect : DestroyerAction, IEffectSpawnAction
+{
+    protected override void OnInvoke()
+    {
+        // No effect action is performed
+    }
+}
