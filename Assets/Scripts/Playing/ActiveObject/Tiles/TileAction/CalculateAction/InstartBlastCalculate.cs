a2b80d2a00015bf3a8823b26a814708a55a26796
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-08-08 01:05:21 +0900

    폭발 타일용 계산 액션 정의

---
+++ Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/InstartBlastCalculate.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstartBlastCalculate : TileAction, ICalculateAction
+{
+    public Vector2Int isCalculated { get; set; } = Vector2Int.zero;
+
+    protected override void OnInvoke()
+    {
+        tile.Blast();
+    }
+
+    public void CalReset()
+    {
+        isCalculated = Vector2Int.zero;
+        tile.isCenter = false;
+        tile.length = Vector2Int.zero;
+    }
+
+    public void NearbyCheck(ref Vector2Int length, ref Stack<Tile> totalStack, Vector2Int exceptionDirection)
+    {
+    }
+    public bool IsEqualType(TileType type)
+    {
+        return tile.tileType == type;
+    }
+}
