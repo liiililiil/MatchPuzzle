d9229e68f9404bd9b7bd76b210f90c3a4bbc8dfd
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-08-08 01:00:19 +0900

    문법 수정

--- Assets/Scripts/Playing/ActiveObject/IActiveObject.cs
+++ Assets/Scripts/Playing/ActiveObject/IActiveObject.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public interface IActiveObject
 {
-    public bool isActive { get;}
+    public bool isActive { get; }
     public void Disable();
     public void Enable(Vector2 position, Quaternion rotate, IActiveObject startBy = null);
 }
