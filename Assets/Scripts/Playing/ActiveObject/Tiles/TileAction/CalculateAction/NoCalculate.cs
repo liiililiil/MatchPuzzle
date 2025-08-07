af34c9322a17f9ab68b76fa4dd320807007f1d37
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-08-08 01:05:48 +0900

    계산없는 타일 클래스 정의

--- Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/NoCalculate.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/NoCalculate.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class NoCalculate : TileAction, ICalculateAction
 {
-    public bool isCalculated { get; set; } = false;
+    public Vector2Int isCalculated { get; set; } = Vector2Int.zero;
 
     protected override void OnInvoke()
     {
@@ -11,13 +11,17 @@ public class NoCalculate : TileAction, ICalculateAction
 
     public void CalReset()
     {
-        isCalculated = false;
+        isCalculated = Vector2Int.zero;
         tile.isCenter = false;
         tile.length = Vector2Int.zero;
     }
 
     public void NearbyCheck(ref Vector2Int length, ref Stack<Tile> totalStack, Vector2Int exceptionDirection)
     {
+    }
+    public bool IsEqualType(TileType type)
+    {
+        return tile.tileType == type;
     }
     
 }
