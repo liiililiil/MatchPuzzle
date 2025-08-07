a5ad29bfab8532092212dc7aff5381cdc015109d
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-08-08 01:00:05 +0900

    포커스에 필요한 상수 정의

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -14,10 +14,12 @@ public static class Utils
     public const int TILETYPE_LENGHT = 9;
     public const int EFFECTTYPE_LENGHT = 1;
     public const float EFFECT_SPEED = 20f;
-    public const float DESTROYER_FORWARD_SPEED = 20f;
+    public const float DESTROYER_FORWARD_SPEED = 40f;
     public const float EXTINCTION_DURATION = 1.5f;
     public const float MOUSE_SIZE = 0.2f;
     public const float DESTROYER_TRAIL_EFFECT_SPAWN_SPEED = 0.5f;
+    public const float FOCUS_ANIMATION_MOVING_LENGHT = 0.1f;
+    public const float FOCUS_ANIMATION_SPEED = 10f;
 
     // public const int MOVEMENT_DURATION = 100; // 이동 지속 시간 (ms)
 
