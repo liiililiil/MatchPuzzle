50ea1d2901efe907d0c826773ccd9b2c20908c63
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-08-08 01:02:20 +0900

    디스트로이어의 직선 움직임 정의

--- Assets/Scripts/Playing/ActiveObject/TileDestroyer/DestroyerAction/MovementAction/Straight.cs
+++ Assets/Scripts/Playing/ActiveObject/TileDestroyer/DestroyerAction/MovementAction/Straight.cs
@@ -3,49 +3,29 @@ using UnityEngine;
 
 public class StraightDestroyer : DestroyerAction, IMovementAction
 {
-    private Transform sprite;
-    private Coroutine coroutine;
+    new Rigidbody2D rigidbody2D;
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        if (rigidbody2D == null)
+        {
+            Debug.LogError($"{gameObject.name}에 Rigidbody2D가 없습니다!");
+        }
+    }
     protected override void OnInvoke()
     {
-        if (sprite == null) sprite = transform.GetChild(0).GetComponent<Transform>();
-        coroutine = StartCoroutine(moveMent2D(transform.up));
+        StartCoroutine(Forward());
     }
-        IEnumerator moveMent2D(Vector2 direction)
+
+    IEnumerator Forward()
     {
-        // 현재 위치를 그리드에 정렬
-        Vector2 startPos = new Vector2(
-            Mathf.Round(transform.position.x / Utils.TILE_GAP) * Utils.TILE_GAP,
-            Mathf.Round(transform.position.y / Utils.TILE_GAP) * Utils.TILE_GAP
-        );
-
-        //이동 위치
-        Vector2 targetPos = startPos + direction * Utils.TILE_GAP;
-
-        //히트박스는 미리 이동해놓기 
-        transform.position = targetPos;
-
-        float time = 0f;
-        while (time <= 1f)
+        while (tileDestroyer.isActive)
         {
-            sprite.transform.position = Vector2.Lerp(startPos, targetPos, time);
-            time += Time.deltaTime * Utils.MOVEMENT_SPEED;
+            rigidbody2D.linearVelocity = transform.up * Utils.DESTROYER_FORWARD_SPEED;
             yield return null;
         }
 
-        // 위치 정밀 보정
-        sprite.position = targetPos;
-
-        //하강 완료
-        coroutineEnd();
-    }
-
-    protected void coroutineEnd()
-    {
-        StopCoroutine(coroutine);
-        coroutine = null;
-
-        EventManager.Instance.InvokeDestroyerMove += Invoke;
-        
+        rigidbody2D.linearVelocity = Vector2.zero;
     }
     
 }
