0854132747ee15d706144156beeb1ae45f1ab8b9
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-08-27 21:26:31 +0900

    이펙트를 숨기기위해 인터페이스 함수를 변경

--- Assets/Scripts/Playing/ActiveObject/IActiveObject.cs
+++ Assets/Scripts/Playing/ActiveObject/IActiveObject.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public interface IActiveObject
 {
     public bool isActive { get; }
-    public void Disable();
+    public void Disable(bool hideEffect = false);
     public void Enable(Vector2 position, Quaternion rotate, IActiveObject startBy = null);
 }
