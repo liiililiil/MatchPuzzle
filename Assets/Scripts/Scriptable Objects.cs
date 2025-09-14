8e9f6e1e198427dea81df1f2b9b3c263facad7e0
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-09-15 00:23:06 +0900

    디스트로이어를 스크립터블 오브젝트로 만들어서 활용성 높이기

---
+++ Assets/Scripts/Scriptable Objects.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "DestroyerSpawnPreset", menuName = "Presets/DestroyerSpawn", order = 0)]
+public class DestroyerSpawnPreset : ScriptableObject
+{
+    [SerializeField]
+    private ExplosionType[] destroyers;
+    public ExplosionType[] Destroyers => destroyers;
+   
+}
