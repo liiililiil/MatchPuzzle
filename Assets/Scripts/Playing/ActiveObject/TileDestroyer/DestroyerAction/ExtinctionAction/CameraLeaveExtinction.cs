703639ab67eeb8ebdf9e380896cfa1d66f3b8b5b
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-09-15 02:54:22 +0900

    최적화

--- Assets/Scripts/Playing/ActiveObject/TileDestroyer/DestroyerAction/ExtinctionAction/CameraLeaveExtinction.cs
+++ Assets/Scripts/Playing/ActiveObject/TileDestroyer/DestroyerAction/ExtinctionAction/CameraLeaveExtinction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
+// 카메라 시야에서 벗어나면 타일을 파괴하는 액션
 public class CameraLeaveExtinction : DestroyerAction, IExtinctionAction
 {
     protected override void OnInvoke()
@@ -8,21 +9,19 @@ public class CameraLeaveExtinction : DestroyerAction, IExtinctionAction
         StartCoroutine(LeaveCheck());
     }
 
+    // 카메라 시야에서 벗어났는지 체크하는 코루틴
     IEnumerator LeaveCheck()
     {
         Vector3 viewPos;
-        while (true)
+
+        while (tileDestroyer.isActive)
         {
+            yield return null;
+
             viewPos = Camera.main.WorldToViewportPoint(gameObject.transform.position);
 
             if (viewPos.z < 0 || viewPos.x < 0 || viewPos.x > 1 || viewPos.y < 0 || viewPos.y > 1)
-            {
                 tileDestroyer.Disable();
-                break;
-            }
-            
-            yield return null;
-                
         }
     }
 }
