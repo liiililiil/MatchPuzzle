3f1cfe2981600de0a349f99b5ae406c99cde2070
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-10-26 21:54:14 +0900

    TileCalCulateAction 클래스의 _isCalculated 초기화 및 CalReset 메서드 수정

--- Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/TileCalCulateAction.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/TileCalCulateAction.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class TileCalCulateAction : TileAction
 {
-    protected Vector2Int _isCalculated;
-    public Vector2Int isCalculated { get => _isCalculated; set => _isCalculated = value; }
+    // 연산이 완료되었는지 여부
+    protected Vector2Int _isCalculated = Vector2Int.zero;
+
+    //밖 접근용
+    public Vector2Int isCalculated
+    {
+        get => _isCalculated;
+        set => _isCalculated = value;
+    }
     public void CalReset()
     {
         isCalculated = Vector2Int.zero;
-        tile.length = Vector2Int.zero;
         tile.isCenter = false;
+        tile.length = Vector2Int.one;
     }
+
+    // 외부 접근용 타입 비교
     public bool IsEqualType(TileType type)
     {
         return tile.tileType == type;
     }
+
 }
