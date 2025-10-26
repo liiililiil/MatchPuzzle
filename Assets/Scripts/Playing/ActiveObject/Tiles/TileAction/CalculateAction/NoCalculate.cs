574d02afe6513e7582f77562306cededf20c8740
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-10-26 21:53:47 +0900

    NoCalculate 클래스의 상속 수정 및 불필요한 메서드 제거

--- Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/NoCalculate.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/NoCalculate.cs
@@ -2,27 +2,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 계산하지 않는 액션
-public class NoCalculate : TileAction, ICalculateAction
+public class NoCalculate : TileCalCulateAction, ICalculateAction
 {
-    public Vector2Int isCalculated { get; set; } = Vector2Int.zero;
 
     protected override void OnInvoke()
     {
     }
-
-    public void CalReset()
-    {
-        isCalculated = Vector2Int.zero;
-        tile.isCenter = false;
-        tile.length = Vector2Int.zero;
-    }
-
     public void NearbyCheck(ref Vector2Int length, ref Stack<Tile> totalStack, Vector2Int exceptionDirection)
     {
-    }
-    public bool IsEqualType(TileType type)
-    {
-        return tile.tileType == type;
     }
 
 }
