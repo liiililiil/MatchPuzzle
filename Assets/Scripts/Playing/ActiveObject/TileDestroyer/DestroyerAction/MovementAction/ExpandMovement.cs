73ed7f5ae42be3ecb4cc0c088dab0278178c5fa0
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-10-26 21:50:20 +0900

    ExpandMovement 클래스 추가 및 스케일 증가 코루틴 구현

---
+++ Assets/Scripts/Playing/ActiveObject/TileDestroyer/DestroyerAction/MovementAction/ExpandMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class ExpandMovement : DestroyerAction, IMovementAction
+{
+    [SerializeField]
+    private Vector2 initialScale;
+    [SerializeField]
+    private Vector2 targetScale;
+    protected override void OnInvoke()
+    {
+        StartCoroutine(ScaleIncrease());
+    }
+
+        IEnumerator ScaleIncrease()
+    {
+        float time = 0f;
+        while (time < 1f)
+        {
+            time += Time.deltaTime * Utils.SHOOTER_SCALE_SPEED;
+            tileDestroyer.gameObject.transform.localScale = Vector2.Lerp(initialScale, targetScale, time);
+            yield return null;
+        }
+        tileDestroyer.gameObject.transform.localScale = targetScale;
+    }
+
+}
