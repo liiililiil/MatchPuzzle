fa4823a790dabee3b4925c380fffc006187ae843
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-10-26 21:53:44 +0900

    InstartBlastCalculate 클래스의 상속 수정 및 불필요한 메서드 제거

--- Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/InstartBlastCalculate.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/CalculateAction/InstartBlastCalculate.cs
@@ -2,9 +2,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 즉시 폭발하는 액션
-public class InstartBlastCalculate : TileAction, ICalculateAction
+public class InstartBlastCalculate : TileCalCulateAction, ICalculateAction
 {
-    public Vector2Int isCalculated { get; set; } = Vector2Int.zero;
 
     // 즉시 폭발
     protected override void OnInvoke()
@@ -12,22 +11,10 @@ public class InstartBlastCalculate : TileAction, ICalculateAction
         if (tile.switchedTileType != TileType.Empty) tile.Blast();
     }
     
-    // 연산 초기화
-    public void CalReset()
-    {
-        isCalculated = Vector2Int.zero;
-        tile.isCenter = false;
-        tile.length = Vector2Int.zero;
-    }
 
     // 주변 타일 검사 (사용 안함)
     public void NearbyCheck(ref Vector2Int length, ref Stack<Tile> totalStack, Vector2Int exceptionDirection)
     {
     }
 
-    // 외부 접근용 타입 비교
-    public bool IsEqualType(TileType type)
-    {
-        return tile.tileType == type;
-    }
 }
