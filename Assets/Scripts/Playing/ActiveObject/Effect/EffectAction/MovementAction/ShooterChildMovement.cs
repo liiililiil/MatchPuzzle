5843d9d9ee3a1eb67fabe4beae8a290cf2e7d84b
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-10-26 21:49:30 +0900

    컬러폭탄 이펙트 움직임 클래스 생성

---
+++ Assets/Scripts/Playing/ActiveObject/Effect/EffectAction/MovementAction/ShooterChildMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShooterChildMovement : EffectAction, IEffectMovementAction
+{
+    public Vector2 targetPosition;
+    protected override void OnInvoke()
+    {
+        // Debug.Log("슈터 자식 이동 액션 시작", this);
+
+        StartCoroutine(Animation());
+    }
+
+    IEnumerator Animation()
+    {
+        float time = 0f;
+        Vector2 startPosition = transform.position;
+        Vector2 firstTargetPosition = startPosition + new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        Vector2 targetPos = Vector2.zero;
+
+        float angle = Mathf.Atan2(firstTargetPosition.y - startPosition.y, firstTargetPosition.x - startPosition.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        while (time < 1f)
+        {
+            time += Time.deltaTime * Utils.SHOOTER_CHILD_MOVE_SPEED;
+
+            //움직임
+            if (time < 0.5f)
+            {
+                targetPos = Vector2.Lerp(startPosition, firstTargetPosition, EaseMoveMent.Ease(EaseType.OutExpo, time * 2f));
+            }
+            else if (time >= 0.5f)
+            {
+                targetPos = Vector2.Lerp(firstTargetPosition, targetPosition, EaseMoveMent.Ease(EaseType.InExpo, (time - 0.5f) * 2f));
+            }
+            
+            //회전
+            if(time > 0.3f)
+            {
+                float targetAngle = Mathf.Atan2(targetPosition.y - firstTargetPosition.y, targetPosition.x - firstTargetPosition.x) * Mathf.Rad2Deg;
+
+                transform.rotation = Quaternion.Euler(0, 0, Mathf.LerpUnclamped(angle , targetAngle, EaseMoveMent.Ease(EaseType.InOutExpo, (time - 0.3f) *2f)));
+            }
+            
+            transform.position = targetPos;
+            yield return null;
+            
+        }
+
+        transform.position = targetPosition;
+    }
+}
