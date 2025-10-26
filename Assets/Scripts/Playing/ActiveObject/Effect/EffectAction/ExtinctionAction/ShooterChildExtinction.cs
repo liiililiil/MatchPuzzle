429ce1a8ac7eab5d37b75553e78cb4761568d093
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-10-26 21:49:16 +0900

    컬러폭탄 이벡트 소멸 클래스 생성

---
+++ Assets/Scripts/Playing/ActiveObject/Effect/EffectAction/ExtinctionAction/ShooterChildExtinction.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShooterChildExtinction : EffectAction, IEffectExtinctionAction
+{
+    protected override void OnInvoke()
+    {
+        StartCoroutine(startExtinctionTimer());
+    }
+    
+    IEnumerator startExtinctionTimer()
+    {
+        float time = 0f;
+        while (time <= 1)
+        {
+            time += Time.deltaTime * Utils.SHOOTER_CHILD_MOVE_SPEED;
+            yield return null;
+            // Debug.Log(time);
+        }
+
+        // 1 프레임 대기  
+        yield return null;
+        effect.Disable();
+    }
+}
