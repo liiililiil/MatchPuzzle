db09e949c661ff717bf1affbcb71dd99aa22ad40
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-10-26 21:54:34 +0900

    Move 메서드에 isOperand 매개변수 추가

--- Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/TIleFocusAction/ITileFocusAction.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/TIleFocusAction/ITileFocusAction.cs
@@ -7,5 +7,5 @@ public interface ITileFocusAction : ITileAction
     public bool isCanFocus { get; }
     public void OnFocus(Vector2Int at);
     public void UnFocus();
-    public void Move(Vector2Int moveTo);
+    public void Move(Vector2Int moveTo, bool isOperand = false);
 }
