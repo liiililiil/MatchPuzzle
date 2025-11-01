43f2eaa7cf38f5a528439a7babb4c0cf039c7712
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-11-02 07:37:31 +0900

    코드 위치를 조정해 성능을 약간 개선하고 버그를 임시로 수정하기 위한 코드를 추가하였습니다.

--- Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/DropAction/DropAction.cs
+++ Assets/Scripts/Playing/ActiveObject/Tiles/TileAction/DropAction/DropAction.cs
@@ -9,6 +9,23 @@ public class DropAction : TileAction
     public override void Invoke()
     {
 
+        if (!tile.isActive)
+        {
+            Debug.LogWarning($"{gameObject.name}에 있는{GetType()}가 비활성 상태이므로 Invoke를 무시합니다.");
+            return;
+        }
+
+        /// <summary>
+        /// 이 코드는 임시 코드입니다. 차후 원인 발견 후 수정될 예정입니다.
+        /// </summary>
+        if ((Vector2)transform.position == new Vector2(Utils.WAIT_POS_X, Utils.WAIT_Pos_Y))
+        {
+            Debug.LogWarning($"{gameObject.name}이(가) 대기 위치에 있으므로 Invoke를 무시합니다.");
+            return;
+        }
+        
+
+
         if (tile == null)
         {
             tile = GetComponent<Tile>();
@@ -20,12 +37,6 @@ public class DropAction : TileAction
             }
 
             Debug.LogWarning($"{gameObject.name}에 있는 {GetType()}가 Init되지 않았습니다. 자동으로 Init합니다.");
-        }
-
-        if (!tile.isActive)
-        {
-            // Debug.LogWarning($"{gameObject.name}에 있는{GetType()}가 비활성 상태이므로 Invoke를 무시합니다.");
-            return;
         }
 
         OnInvoke();
