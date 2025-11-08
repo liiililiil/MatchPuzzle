6cb8fba0c600e4effebc5645b29d848ade4ad993
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-11-08 15:28:29 +0900

    fps 표시기 최적화

--- Assets/Scripts/FpsShower.cs
+++ Assets/Scripts/FpsShower.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FpsShower : MonoBehaviour
 {
+    StringBuilder stringBuilder = new StringBuilder();
     public Text text;
     float time;
     void Start()
@@ -15,11 +17,11 @@ public class FpsShower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        if(time >0.1f){
-            text.text = $"{1f/Time.deltaTime}, Fps ({Time.deltaTime * 1000})ms";
-            time = 0;
-        }
-        
+        stringBuilder.Clear();
+        stringBuilder.Append(1f / Time.deltaTime);
+        stringBuilder.Append(", Fps (");
+        stringBuilder.Append(Time.deltaTime * 1000);
+        stringBuilder.Append(")ms");
+        text.text = stringBuilder.ToString();
     }
 }
