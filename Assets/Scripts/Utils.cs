d1ec54ad72d5dfd44a44eb31139d313706c184f1
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-11-12 01:41:31 +0900

    속도제어와 관련된 변수를 분리

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -3,8 +3,6 @@ using System.Runtime.CompilerServices;
 using System;
 public static class Utils
 {
-    // 전체 속도에 대한 배율
-    public const float SPEED_MULTIPLY = 3f;
 
 
     public const float TILE_SIZE = 0.9f;
@@ -12,20 +10,17 @@ public static class Utils
     public const float RAYCASY_LENGHT = 0.2f;
     public const float RAYCASY_REVISION = 1f;
     public const float WAIT_POS_X = 100f;
-    public const float WAIT_Pos_Y = 100f;
-    public const float MOVEMENT_SPEED = 20f * SPEED_MULTIPLY;
+    public const float WAIT_POS_Y = 100f;
     public const int TILETYPE_LENGHT = 9;
     public const int EFFECTTYPE_LENGHT = 1;
-    public const float EFFECT_DURATION = 15f * SPEED_MULTIPLY;
-    public const float DESTROYER_FORWARD_SPEED = 20f * SPEED_MULTIPLY;
     public const float EXTINCTION_DURATION = 1.5f;
     public const float MOUSE_SIZE = 0.2f;
-    public const float DESTROYER_TRAIL_EFFECT_SPAWN_SPEED = 0.5f * SPEED_MULTIPLY;
     public const float FOCUS_ANIMATION_MOVING_LENGHT = 0.1f;
-    public const float FOCUS_ANIMATION_SPEED = 10f * SPEED_MULTIPLY;
-    public const float FOCUS_MOVE_SPEED = 10f * SPEED_MULTIPLY;
-    public const float SHOOTER_SCALE_SPEED = 1f * SPEED_MULTIPLY;
-    public const float SHOOTER_CHILD_MOVE_SPEED = 1f * SPEED_MULTIPLY;
+
+    public const float GAME_SPEED_MAX = 3f;
+
+
+
 
     // public const int MOVEMENT_DURATION = 100; // 이동 지속 시간 (ms)
 
