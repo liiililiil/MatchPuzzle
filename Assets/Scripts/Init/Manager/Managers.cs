131fa14c8d14c57a2aea3b0708dd394bd7011124
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-11-17 04:02:45 +0900

    매니저 관리를 위한 통헙화

---
+++ Assets/Scripts/Init/Manager/Managers.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public abstract class Managers<T> : MonoBehaviour where T : Managers<T>
+{
+    public static T Instance;
+    protected void Awake()
+    {
+        // 싱글톤
+        if (Instance == null)
+        {
+            Instance = (T)this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != (T)this)
+        {
+            Destroy(gameObject);
+        }
+
+        OnAwake();
+    }
+
+    protected virtual void OnAwake()
+    {
+        
+    }
+
+}
