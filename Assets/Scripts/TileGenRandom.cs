df2498b710c357124fec4ca83c514af972e275e6
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-11-17 04:01:40 +0900

    타일 생성난수를 위한 클래스 생성

---
+++ Assets/Scripts/TileGenRandom.cs
@@ -0,0 +1,16 @@
+public static class TileGenRandom
+{
+    private static int buffer;
+
+    // private const byte WEIGHT_COUNT = Utils.;
+
+    public static void Reset(int seed)
+    {
+        
+    }
+
+    // public int Random()
+    // {
+        
+    // } 
+}
