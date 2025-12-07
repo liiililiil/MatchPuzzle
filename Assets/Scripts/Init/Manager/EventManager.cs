9ce4ff494ed3aab3a4c33ed88d6ede80964df88e
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-12-08 05:30:19 +0900

    생명 주기를 페이즈로 변경

--- Assets/Scripts/Init/Manager/EventManager.cs
+++ Assets/Scripts/Init/Manager/EventManager.cs
@@ -3,6 +3,16 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
+
+enum Phase
+{
+    Drop,
+    Focus,
+    Calculate,
+    Blast,
+    DestroyerWait
+}
+
 public class EventManager : Managers<EventManager>
 {
     public OneTimeAction InvokeCalReset = new OneTimeAction();
@@ -32,52 +42,99 @@ public class EventManager : Managers<EventManager>
     [HideInInspector]
     public SimpleEvent<IActiveObject, Vector2> OnSpawnedActiveOjbect = new SimpleEvent<IActiveObject, Vector2>();
 
-    public int movingTiles;
     public int dropTiles;
     public int moveTestTiles;
     public bool readyToFocus;
     public bool NeedTestCalculation;
     public int activeDestroyer;
 
+    [SerializeField]
+    private Phase phase = 0;
+    
     public void MoveTest()
     {
         StartCoroutine(MoveTestWait());
     }
 
     private void FixedUpdate() {
-        //포커싱 될 준비가 끝나면 포커싱 대기
-        if (readyToFocus == true)
+        switch (phase)
         {
-            return;
+            case Phase.Drop:
+                DropPhase();
+                break;
+            case Phase.Focus:
+                FocusPhase();
+                break;
+            case Phase.Calculate:
+                CalculatePhase();
+                break;
+            case Phase.Blast:
+                BlastPhase();
+                break;  
+            case Phase.DestroyerWait:
+                DestroyerWaitPhase();
+                break;
         }
+    }
 
+    private void DropPhase()
+    {
         GameSpeedManager.Instance.StartSpeedIncrease();
-        
-        InvokeBlast.Invoke();
 
-        if (activeDestroyer <= 0 && dropTiles <= 0)
-        {   
-            
-            InvokeSpawnTile.Invoke();   
+        InvokeDrop.Invoke();
+        InvokeSpawnTile.Invoke();   
 
-            if (dropTiles <= 0)
-            {
-                InvokeDrop.Invoke();
-            }
-
-
-            if (movingTiles <= 0)
-            {
-                InvokeCalReset.Invoke();
-                InvokeCalculate.Invoke();
-
-                if (InvokeBlast.Count <= 0)
-                {
-                    GameSpeedManager.Instance.StopSpeedIncrease();
-                    readyToFocus = true;
-                }
-            }
+        if(dropTiles <= 0 && InvokeDrop.Count <= 0)
+        {
+            phase = Phase.Calculate;
         }
+    }
+
+    private void BlastPhase()
+    {
+        InvokeBlast.Invoke();
+        if(activeDestroyer > 0)
+        {
+            phase = Phase.DestroyerWait;
+        }
+        else
+        {
+            phase = Phase.Drop;
+            
+        }
+    }
+
+    private void DestroyerWaitPhase()
+    {
+        if(activeDestroyer <= 0)
+        {
+            phase = Phase.Drop;
+        }
+        else
+        {
+            InvokeBlast.Invoke();
+        }
+    }
+
+    private void CalculatePhase()
+    {
+        InvokeCalReset.Invoke();
+        InvokeCalculate.Invoke();
+
+        if (InvokeBlast.Count <= 0)
+        {
+            phase = Phase.Focus;
+        }
+        else
+        {
+            phase = Phase.Blast;
+        }
+    }
+
+    private void FocusPhase()
+    {
+        GameSpeedManager.Instance.StopSpeedIncrease();
+        readyToFocus = true;
     }
         
 
@@ -104,6 +161,8 @@ public class EventManager : Managers<EventManager>
             InvokeReMove.Clear();
             InvokeFocusBlast.Invoke();
             readyToFocus = false;
+
+            phase = Phase.Blast;
         }
     }
 
