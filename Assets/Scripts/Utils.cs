40ef7e6b96bd62443ee9d3765fa7652f7c1acf37
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-12-08 05:25:32 +0900

    타일 탐색과 정리를 유틸로 이동, 타일 갭 수정

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -6,9 +6,8 @@ public static class Utils
 
 
     public const float TILE_SIZE = 0.9f;
-    public const float TILE_GAP = 1f;
+    public const float TILE_GAP = 1.5f;
     public const float RAYCASY_LENGHT = 0.2f;
-    public const float RAYCASY_REVISION = 1f;
     public const float WAIT_POS_X = 100f;
     public const float WAIT_POS_Y = 100f;
     public const int TILETYPE_LENGHT = 9;
@@ -44,5 +43,59 @@ public static class Utils
         throw new NotImplementedException();
     }
 
+    //4방향으로 타일을 검사하여 정리 요청
+    public static void CallOrganize(GameObject target)
+    {
+        //4 방향으로 Ray 쏘기
+        foreach (Vector2 dir in Utils.directions)
+        {
+            // 상대 방향을 절대 방향으로 변환
+            Vector2 worldDir = target.transform.TransformDirection(dir);
 
+            // GetTileFromWorld를 이용해 타일을 찾고 정리 수행
+            Tile tile = Utils.TryGetTile(target.transform.position, worldDir, 1);
+            tile?.Organize();
+        }
+    }
+    
+    //모듈 탐색
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T TryGetAction<T>(Vector2 start, Vector2 direction, float distance, bool ignoreBlock = false) where T : class, ITileAction
+    {
+        return TryGetTile(start, direction, distance, ignoreBlock)?.GetComponent<T>();
+    }
+
+    //타일만 탐색
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Tile TryGetTile(Vector2 start, Vector2 direction, float distance, bool ignoreBlock = false){
+        return TryGetGameObject(start, direction, distance, LayerMask.GetMask("Tile"), ignoreBlock)?.GetComponent<Tile>();
+    }
+
+
+    // 게임 오브젝트 탐색
+    public static GameObject TryGetGameObject(Vector2 start, Vector2 direction, float distance, int layer, bool ignoreBlock = false)
+    {
+        Vector2 startPos = start + direction * ((Utils.TILE_SIZE/2) + 0.1f);
+        float targetDistance = Mathf.Max(distance - ((Utils.TILE_SIZE/2) + 0.1f), 0.1f);
+        
+    #if UNITY_EDITOR
+            DrawRayDebug(startPos, direction, targetDistance);
+    #endif
+        if (ignoreBlock)
+        {
+            layer |= LayerMask.GetMask("Block");
+        }
+            RaycastHit2D hit = Physics2D.Raycast(startPos, direction, targetDistance, layer);
+            return hit.collider?.gameObject;
+    }
+        
+        
+        
+    #if UNITY_EDITOR
+        public static void DrawRayDebug(Vector2 center, Vector2 direction, float distance)
+        {
+            Debug.DrawRay(center, direction * distance, Color.red, 0.1f);
+        }
+    #endif
 }
+
