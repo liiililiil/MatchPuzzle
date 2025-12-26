908ba43f28d130652cd9fe036407a5d61ce19447
Tivm <109393040+liiililiil@users.noreply.github.com>
2025-12-26 21:03:15 +0900

    메뉴 상태를 숫자로 표기,  메뉴 메니저를 싱글톤에서 해제

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -5,46 +5,54 @@ using UnityEngine;
 
 [Serializable]
 public enum MenuState{
-    Main,
-    Setting,
-    Warning,
-    Select,
-    MiniGame,
-    inGame
+    Main = 0,
+    Setting = 1,
+    Warning = 2,
+    Select = 3,
+    MiniGame = 4,
+    InGame = 5,
+    End = 6,
 }
 
 
-public class MenuManager : Managers<MenuManager>
+public class MenuManager : MonoBehaviour
 {
+    public static MenuManager Instance { get; private set; }
+    
     public SimpleEvent<MenuState> OnChangeMenuState = new SimpleEvent<MenuState>();
+
+    public MenuState currentMenu = MenuState.Main;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     private void Start()
     {
+
         StartCoroutine(delayedInvoke());
+        OnChangeMenuState.AddListener(MenuChangeHandler);
     }
 
     IEnumerator delayedInvoke()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         // Debug.Log("MenuManager Start Invoke Main");
         OnChangeMenuState.Invoke(MenuState.Main);
     }
 
-    ~MenuManager()
+    private void MenuChangeHandler(MenuState menuState)
     {
-        try
-        {
-            Instance = null;
-        }
-        catch (Exception)
-        {
-            // 무시
-        }
-        
+        currentMenu = menuState;
+
     }
 
-    public void ChangeMenu(MenuState menuState)
+    public void ChangeMenu(int menuState)
     {
-        OnChangeMenuState.Invoke(menuState);
+        OnChangeMenuState.Invoke((MenuState)menuState);
+        // Debug.Log("Menu Changed to " + (MenuState)menuState);
     }
+
+
 
 }
