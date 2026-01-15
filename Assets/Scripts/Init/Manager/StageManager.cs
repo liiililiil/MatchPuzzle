fbbd5fd77c0ff0a097810e3d05417d3269540819
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-16 04:58:11 +0900

    기본적인 스테이지 제어 메니저 추가

--- Assets/Scripts/Init/Manager/StageManager.cs
+++ Assets/Scripts/Init/Manager/StageManager.cs
@@ -4,26 +4,58 @@ public class StageManager : Managers<StageManager>
 {
     
     public IInStageManager inStageManager { get; private set; }
+
+    [SerializeField]
+    private GameObject failGameObject;
+
+    private byte caller;
+
+    private SimpleEvent OnFail = new SimpleEvent();
+    private SimpleEvent OnSuccess = new SimpleEvent();
+    private SimpleEvent OnStageStart = new SimpleEvent();
+    private SimpleEvent OnStageClose = new SimpleEvent();
     
-    private void StageStart()
+    //버튼에서 쓰는 함수들
+    public void StageStart()
     {
+        EventManager.Instance.ForceRelease(caller);
 
+        OnStageStart.Invoke();
     }
 
-    private void StageClose()
+    public void StageClose()
     {
+        failGameObject.SetActive(false);
 
+        OnStageClose.Invoke();
+        SceneLoadManager.Instance.SceneLoad("Menu", () => MenuManager.Instance.ChangeMenu(3));
     }
 
-    public void Fail()
+
+    //InStageManager에서 쓰는 함수
+    public void GameStart(bool isSilence = false)
     {
-        Debug.Log("Stage Fail");
+        EventManager.Instance.ForcePause(out caller);
+
+        OnStageStart.Invoke();
+    }
+  
+    public void Fail(string cause, bool isSilence = false)
+    {
+        EventManager.Instance.ForcePause(out caller);
+        failGameObject.GetComponent<GameOverDisplay>().DisplayedGameOver(cause,inStageManager.stagePrograss.tileRecode);
+
+        OnFail.Invoke();
     }
 
     public void Success()
     {
-        Debug.Log("Stage Success");
+        EventManager.Instance.ForcePause(out caller);
+
+        OnSuccess.Invoke();
     }
+
+
 
     public void bind(IInStageManager inStageManager_)
     {
