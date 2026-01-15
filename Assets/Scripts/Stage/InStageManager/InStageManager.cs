8c2011d8c00cb2fd635868b11ce52310233c922f
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-16 05:00:26 +0900

    스테이지 목표의 캡슐화를 해제, 실패 이유 작성, OnStart를 오버라이드하여 사용하도록 변경

--- Assets/Scripts/Stage/InStageManager/InStageManager.cs
+++ Assets/Scripts/Stage/InStageManager/InStageManager.cs
@@ -8,14 +8,16 @@ public abstract class InStageManager : MonoBehaviour
     public StagePrograss stagePrograss { get; protected set; } = new StagePrograss();
 
     [SerializeField]
-    protected StageClearTargetPreset stageClearTarget{ get; set; }
-
+    protected StageClearTargetPreset stageClearTarget;
     private void Start() {
         StageManager.Instance.bind(this as IInStageManager);
         OnStart();
     }
 
-    protected abstract void OnStart();
+    protected virtual void OnStart()
+    {
+        Debug.Log("시작시 명령이 설정되지 않음");
+    }
 
     protected void Init(int score_, int leftMovement_)
     {
@@ -36,7 +38,7 @@ public abstract class InStageManager : MonoBehaviour
         
         if(stagePrograss.leftMovement != -1 && stagePrograss.leftMovement <= 0)
         {
-            StageManager.Instance.Fail();
+            StageManager.Instance.Fail("남은 움직임 없음!");
             return;
         }
     }
